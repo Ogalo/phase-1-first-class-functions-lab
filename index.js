be46482adf3289b7e15e36121dd97f86f120e3ed
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-06-09 23:24:19 +0300

    commit tree a296d7ccbeef11e43948d5cab7fbeee433a4197d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+    return arrayOfDrivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+    return arrayOfDrivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(number) {
+    return function(value) {
+        return (number * value);
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
